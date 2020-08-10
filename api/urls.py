3ba98a2af56ce414b8377e8c96f1ecab36a5fa4d
TARANVIR SINGH SAINI <taranvir.554@gmail.com>
2020-08-11 03:00:14 +0530

    Commit

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+
+from api.views import MovieViewSet, RatingViewSet, UserViewSet
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
